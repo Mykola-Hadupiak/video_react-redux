f65b82c7a65d076a161e901b7ef668881c4d4741
Mykola Hadupiak <mykola.hadupiak@gmail.com>
2023-12-30 16:38:19 +0200

    before FULL use of redux/toolkit

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,14 @@
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
-import './app/store';
-
-// npm i react-redux @types/react-redux
+import store from './app/store';
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
 root.render(
-  <App />
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
